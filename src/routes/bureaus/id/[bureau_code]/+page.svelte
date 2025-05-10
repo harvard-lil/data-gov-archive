33e5a1679632bdff21f17a60d8ec8694b9194bb5
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-05-10 09:09:01 -0800

    Simplify bureau labeling

--- src/routes/bureaus/id/[bureau_code]/+page.svelte
+++ src/routes/bureaus/id/[bureau_code]/+page.svelte
@@ -3,22 +3,19 @@
   import DatasetListItem from "$lib/DatasetListItem.svelte";
 
   let { data } = $props();
+
+  let bureauLabel = $derived(
+    data.bureau_name ? `${data.bureau_name} (${data.bureau_code})` : data.bureau_code
+  );
 </script>
 
 <svelte:head>
-  {#if data.bureau_name}
-    <title>Archive of Data.gov: {data.bureau_name} ({data.bureau_code})</title>
-  {:else}
-    <title>Archive of Data.gov: {data.bureau_code}</title>
-  {/if}
+  <title>Archive of Data.gov: {bureauLabel}</title>
 </svelte:head>
 
 <h2>
-  {#if data.bureau_name}
-    <b>Bureau:</b> {data.bureau_name} ({data.bureau_code})
-  {:else}
-    <b>Bureau:</b> {data.bureau_code}
-  {/if}
+  <b>Bureau:</b>
+  {bureauLabel}
 </h2>
 <DatasetList datasets={data.datasets} />
 
