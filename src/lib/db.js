df46975060d2a3026d564f1b5ced1f523d5a8163
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-05-12 09:45:47 -0800

    Initialize database connection once

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,6 +2,10 @@ import fs from "fs";
 
 import initSqlJs from "sql.js";
 
+const buffer = fs.readFileSync("./data/data.db");
+const sqlJs = await initSqlJs();
+const db = new sqlJs.Database(buffer);
+
 const zipResults = (results) => {
   const [result] = results;
   return result.values.map((values) => {
@@ -12,10 +16,6 @@ const zipResults = (results) => {
 };
 
 export const queryData = async (query) => {
-  const buffer = fs.readFileSync("./data/data.db");
-  const sqlJs = await initSqlJs();
-  const db = new sqlJs.Database(buffer);
-
   const results = db.exec(query);
   return zipResults(results);
 };
