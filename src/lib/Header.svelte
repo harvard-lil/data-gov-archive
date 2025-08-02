79c5d1e1a23626a664e394a39378791552aab32a
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-08-01 17:39:06 -0800

    Update language in page header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,20 @@
 <header>
   <h1><a href="/">Archive of Data.gov</a></h1>
   <p>
-    This is the Harvard University Library Innovation Lab's archive of <a href="https://data.gov">
-      Data.gov
+    This is an access point to the Harvard University <a href="https://lil.law.harvard.edu">
+      Library Innovation Lab's
     </a>
-    datasets. For more information, see the
-    <a href="https://source.coop/repositories/harvard-lil/gov-data"> project homepage </a> or the
+    archive of <a href="https://data.gov">Data.gov</a>
+    datasets. The collection contains 17.9 TB of data across 311,604 datasets harvested, starting in
+    2024, from public data cataloged on Data.gov. For more information, see the About page or the
     <a href="https://lil.law.harvard.edu/blog/2025/02/06/announcing-data-gov-archive/">
       project announcement
     </a> on our blog.
+  </p>
+  <p>
+    You can also browse the <a href="https://source.coop/repositories/harvard-lil/gov-data"
+      >entire dataset on Source Cooperative</a
+    >.
   </p>
 </header>
 
