437a9f931c6a6704a76c7fe44c265f4c538b3133
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-09-21 11:34:13 -0700

    Prerender site using adapter-static

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "./data/data.db",
-          dest: "./data/data.db",
-        },
-      ],
-    }),
-  ],
-  server: {
-    fs: {
-      allow: ["./data"],
-    },
-  },
+  plugins: [sveltekit()],
 });
