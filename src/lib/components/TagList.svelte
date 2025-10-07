0196f3ebd155159dd8b51ab5528b4cf0ce7c86d2
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-07 12:16:45 -0700

    Tailwind-ify tag list

--- src/lib/components/TagList.svelte
+++ src/lib/components/TagList.svelte
@@ -3,45 +3,16 @@
   let { tags } = $props();
 </script>
 
-<ul>
+<ul class="flex flex-wrap gap-1.5">
   {#each tags as tag}
-    <li>
-      <a href={`${base}/?resource=tags/${encodeURIComponent(tag)}`} title={tag}>
+    <li class="flex-none">
+      <a
+        class="font-mono py-1.5 px-2.5 text-sm text-inherit no-underline hover:underline block max-w-100 overflow-nowrap overflow-hidden text-ellipsis text-nowrap border border-dotted border-slate-800 rounded-md"
+        href={`${base}/?resource=tags/${encodeURIComponent(tag)}`}
+        title={tag}
+      >
         {tag}
       </a>
     </li>
   {/each}
 </ul>
-
-<style lang="scss">
-  ul {
-    padding: 0;
-    list-style: none;
-    list-style-position: unset;
-
-    li {
-      display: inline-block;
-      margin: 0 0.5em 0.25em 0;
-    }
-
-    a {
-      color: inherit;
-      text-decoration: none;
-      display: inline-block;
-      border-radius: 0.25em;
-      border: 1px dotted #222;
-      padding: 0.5em 0.75em;
-      font-family: monospace;
-      max-width: 20em;
-      text-wrap-mode: nowrap;
-      text-wrap-style: pretty;
-      overflow: hidden;
-      text-overflow: nowrap;
-      text-overflow: ellipsis;
-    }
-
-    a:hover {
-      text-decoration: underline;
-    }
-  }
-</style>
