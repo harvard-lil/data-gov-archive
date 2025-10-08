ed3b8663bec9dfad1b36d87abea8ac1261926960
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-08 07:29:06 -0700

    Limit max length of search string

--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -49,6 +49,7 @@
       id="search"
       class="rounded-sm flex-1 p-4 bg-inherit border-slate-800 border-1 border-dotted focus:border-1 focus:border-solid focus:outline-2 focus:outline-slate-800/20 whitespace-nowrap overflow-hidden text-ellipsis placeholder:text-slate-800/50"
       type="text"
+      maxLength="100"
       bind:value={searchQuery}
       onkeydown={handleKeyDown}
       placeholder="Search by dataset title, organization, publisher, bureau, description…"
