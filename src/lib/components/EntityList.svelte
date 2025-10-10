437a6c2e1a549a2450b9de20e89e4db2c9726a3b
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-09 21:09:29 -0700

    Tailwind-ify entity list

--- src/lib/components/EntityList.svelte
+++ src/lib/components/EntityList.svelte
@@ -6,46 +6,18 @@
 
 <ul>
   {#each instances as instance}
-    <li>
-      <h2>
-        <a href={`${base}/?resource=${entity.route}/${encodeURIComponent(instance.identifier)}`}>
-          {instance.label ? instance.label : instance.identifier}
+    <li class="py-6 last:border-b border-t border-dotted border-slate-800 wrap-break-word">
+      <div class="mb-2">
+        <a
+          class="text-inherit text-lg font-bold no-underline hover:underline"
+          href={`${base}/?resource=${entity.route}/${encodeURIComponent(instance.identifier)}`}
+        >
+          {instance.identifier}
         </a>
-      </h2>
-      <p>
+      </div>
+      <p class="text-base italic">
         {Number(instance.count[0]).toLocaleString("en-US")} dataset{#if Number(instance.count[0]) != 1}s{/if}
       </p>
     </li>
   {/each}
 </ul>
-
-<style lang="scss">
-  ul {
-    padding: 0;
-    list-style: none;
-    list-style-position: unset;
-
-    li {
-      margin: 1em 0;
-
-      h2 {
-        font-weight: 700;
-      }
-
-      p {
-        font-style: italic;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-      a:hover {
-        text-decoration: underline;
-      }
-    }
-    li:not(:first-child) {
-      border-top: 1px dotted #222;
-    }
-  }
-</style>
