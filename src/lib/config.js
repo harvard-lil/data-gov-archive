ec05889b1d467182b6876ecf33e411d08a1618f3
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-09 20:55:04 -0700

    Misc. UI tweaks

--- src/lib/config.js
+++ src/lib/config.js
@@ -9,6 +9,3 @@ export const PAGE_SIZE = 25;
 
 // Database settings
 export const DATA_URL = "https://data.source.coop/harvard-lil/staging-gov-data/data";
-
-// UI settings
-// Add other UI-related config here if needed
