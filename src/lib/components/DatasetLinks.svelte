58469dfe4d53f60693d0f651e8a0dc4587b2562f
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-15 23:37:00 -0400

    Swap Lucide in for Material Icons

--- src/lib/components/DatasetLinks.svelte
+++ src/lib/components/DatasetLinks.svelte
@@ -1,34 +1,36 @@
 <script>
+  import { ExternalLink, FileJson, FolderArchive } from "lucide-svelte";
+
   let { dataset } = $props();
 </script>
 
-{#snippet link(icon, href, label)}
+{#snippet link(href, label)}
   <li>
     <a
       class="flex items-center justify-center gap-2 w-full text-inherit text-base no-underline border border-slate-800 rounded-md py-3 px-4 mb-1.5"
       {href}
       target="_blank"
     >
-      <span class="font-material-icons select-none">{icon}</span>
-      <span>{label}</span>
+      {#if label === "Dataset & Metadata (ZIP)"}
+        <FolderArchive size={16} strokeWidth={1.5} absoluteStrokeWidth />
+      {:else if label === "Metadata Only (JSON)"}
+        <FileJson size={16} strokeWidth={1.5} absoluteStrokeWidth />
+      {:else if label === "Data.gov Source"}
+        <ExternalLink size={16} strokeWidth={1.5} absoluteStrokeWidth />
+      {/if}
+      {label}
     </a>
   </li>
 {/snippet}
 
 <ul class="my-4">
   {@render link(
-    "folder_zip",
     `https://source.coop/harvard-lil/gov-data/collections/data_gov/${dataset.name}`,
     "Dataset & Metadata (ZIP)"
   )}
   {@render link(
-    "data_object",
     `https://source.coop/harvard-lil/gov-data/metadata/data_gov/${dataset.name}`,
     "Metadata Only (JSON)"
   )}
-  {@render link(
-    "dataset_linked",
-    `https://catalog.data.gov/dataset/${dataset.name}`,
-    "Data.gov Source"
-  )}
+  {@render link(`https://catalog.data.gov/dataset/${dataset.name}`, "Data.gov Source")}
 </ul>
