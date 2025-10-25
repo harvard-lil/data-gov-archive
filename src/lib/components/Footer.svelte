beaf3523a842c0de3fb9daa41bdb511669aae291
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-10-25 04:45:12 -0700

    Simplify footer markup

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,20 +2,19 @@
   import SkipLink from "./SkipLink.svelte";
 </script>
 
-<footer class="bg-gray-800 text-gray-100 relative">
+<footer class="bg-gray-800 text-gray-100">
   <a id="footer" class="sr-only" tabindex="-1" aria-label="Footer"></a>
-  <SkipLink href="#header" label="top of page" />
 
-  <div class="mx-auto max-w-225 p-6">
-    <div class="max-w-200 flex-1">
-      This is a project of the <a class="text-blue-200" href="https://lil.law.harvard.edu">
-        Library Innovation Lab
-      </a>, part of the
-      <a class="text-blue-200" href="https://hls.harvard.edu"> Harvard Law School Library </a>. For
-      more information on our work, please see the
-      <a class="text-blue-200" href="https://lil.law.harvard.edu/our-work/public-data-project">
-        Public Data Project
-      </a>.
-    </div>
+  <div class="mx-auto max-w-225 p-6 relative">
+    <SkipLink href="#header" label="top of page" />
+
+    This is a project of the
+    <a class="text-blue-200" href="https://lil.law.harvard.edu"> Library Innovation Lab </a>, part
+    of the
+    <a class="text-blue-200" href="https://hls.harvard.edu"> Harvard Law School Library </a>. For
+    more information on our work, please see the
+    <a class="text-blue-200" href="https://lil.law.harvard.edu/our-work/public-data-project">
+      Public Data Project
+    </a>.
   </div>
 </footer>
